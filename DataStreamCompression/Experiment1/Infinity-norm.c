9dc482deabe63ce9b67fdbbc71db3f6f9a5773bd
Bo LI <b.li@motsai.com>
2018-10-17 14:33:07 -0400

    update Experiment1 , readme and travis

--- DataStreamCompression/Experiment1/Infinity-norm.c
+++ DataStreamCompression/Experiment1/Infinity-norm.c
@@ -1,97 +1,146 @@
-#include <stdio.h>
-#include <sys/time.h>
+#include <stdio.h> // printf
+#include <stdlib.h> //malloc
+#include <math.h> // sqrt()
 
-#include "Infinity-norm.h"
+#define DIMENSION_WITHOUT_TIMESTAMP 3
+#define EPSILON (10)
+#define TIME_UNIT_DIFFERENCE 1
 
-#define EPSILON (100)
-#define TIMEUNIT_DIFFERENT 1
+#define MAX(x,y) (x)>=(y)?(x):(y);
+#define MIN(x,y) (x)<=(y)?(x):(y);
+
+struct DATA_POINT {
+    int timestamp;
+    double params[DIMENSION_WITHOUT_TIMESTAMP];
+};
+
+struct BOUND_BOX {
+    /*
+     * [n][0] = upper bound
+     * [n][1] = lower bound
+     */
+    double bounds[DIMENSION_WITHOUT_TIMESTAMP][2];
+};
 
 //#include "local-data-2d.c"
 #include "local-data-3d.c"
 //#include "local-mohamand-2d.c"
 //#include "local-mohamand-3d.c"
 
-struct DATA_POINT base_data;
-struct DATA_POINT coming_data;
-struct CIRCLE corresponse_circle;
-struct LIST_CIRCLE all_circles = {NULL, 0};
-
-void updateBasePoint()
+void initialBoundBox(struct BOUND_BOX *_bound_box, const struct DATA_POINT *_coming_data)
 {
-    int i;
-    for(i=0; i<DIMENSION_WITHOUT_TIMESTAMP; i++)
+    int param_index;
+    for(param_index=0; param_index<DIMENSION_WITHOUT_TIMESTAMP; param_index++)
     {
-        base_data.data.coordinate[i] += (centre_point.coordinate[i] - base_data.data.coordinate[i])
-                *(coming_data.timestamp-1 -base_data.timestamp);
+        _bound_box->bounds[param_index][0] = _coming_data->params[param_index] + EPSILON;
+        _bound_box->bounds[param_index][1] = _coming_data->params[param_index] - EPSILON;
     }
-    base_data.timestamp = coming_data.timestamp-1;
 }
 
-void updateCorCircle()
+void updateBoundBox(struct BOUND_BOX *_bound_box, const struct DATA_POINT *_coming_data, const struct DATA_POINT *_base_data)
 {
-    double ln = coming_data.timestamp - base_data.timestamp;
-    int i;
-    for(i=0; i<DIMENSION_WITHOUT_TIMESTAMP; i++)
+    double time_differ = (_coming_data->timestamp - _base_data->timestamp)/(double)TIME_UNIT_DIFFERENCE;
+
+    //changed_param = (coming_data.ax-base_data.ax)/time_differ + base_data.ax;
+    // bound_box_max = MIN(changed_param + EPSILON/time_differ, bound_box_max);
+    int param_index;
+    for(param_index=0; param_index<DIMENSION_WITHOUT_TIMESTAMP; param_index++)
     {
-        corresponse_circle.centre.coordinate[i] = base_data.data.coordinate[i]
-                + (coming_data.data.coordinate[i] - base_data.data.coordinate[i])/ln;
+        _bound_box->bounds[param_index][0] = MIN(_bound_box->bounds[param_index][0],
+                                                (_base_data->params[param_index] + (_coming_data->params[param_index] - _base_data->params[param_index])/time_differ)
+                                                + EPSILON/time_differ);
+        _bound_box->bounds[param_index][1] = MAX(_bound_box->bounds[param_index][1],
+                                                (_base_data->params[param_index] + (_coming_data->params[param_index] - _base_data->params[param_index])/time_differ)
+                                                - EPSILON/time_differ);
     }
-    corresponse_circle.radius = EPSILON/ln;
 }
+
+void updateBasePoint(const struct BOUND_BOX *_bound_box, const struct DATA_POINT *_coming_data, struct DATA_POINT *_base_data)
+{
+    double time_differ = (_coming_data->timestamp - TIME_UNIT_DIFFERENCE - _base_data->timestamp)/(double)TIME_UNIT_DIFFERENCE;
+
+    _base_data->timestamp = _coming_data->timestamp - TIME_UNIT_DIFFERENCE;
+
+    // new_x = ((bound_box.max_x + bound_box.min_x)/2 - base_data.ax)*time_differ + base_data.ax;
+    int param_index;
+    for(param_index=0; param_index<DIMENSION_WITHOUT_TIMESTAMP; param_index++)
+    {
+        _base_data->params[param_index] += ((_bound_box->bounds[param_index][0] + _bound_box->bounds[param_index][1])/2
+                - _base_data->params[param_index]) * time_differ;
+    }
+}
+
+double dabs(double x){
+    if(x < 0)
+        x = -x;
+    return x;
+}
+
+int isTransmitData(const struct BOUND_BOX *_bound_box, const struct DATA_POINT *_coming_data, const struct DATA_POINT *_base_data)
+{
+    double time_differ = _coming_data->timestamp - _base_data->timestamp;
+
+    int param_index;
+    for(param_index=0; param_index<DIMENSION_WITHOUT_TIMESTAMP; param_index++)
+    {
+        //fabs(changed_x - (bound_box.max_x + bound_box.min_x)/2) > (bound_box.max_x-bound_box.min_x)/2 + EPSILON/ln_base
+        if(dabs(_base_data->params[param_index] + (_coming_data->params[param_index] - _base_data->params[param_index])/time_differ -
+                        (_bound_box->bounds[param_index][0] + _bound_box->bounds[param_index][1])/2)
+                        >  (_bound_box->bounds[param_index][0] - _bound_box->bounds[param_index][1])/2 + EPSILON/time_differ)
+            return 1;
+    }
+    return 0;
+}
+
 
 int main()
 {
-    FILE * f_out = fopen("tmp.csv", "w");
-    int data_index, i;
+    FILE* f_out = fopen("tmp.csv", "w");
+    struct DATA_POINT *coming_data = (struct DATA_POINT *)malloc(sizeof(struct DATA_POINT));
+    struct DATA_POINT *base_data = NULL;
+    struct BOUND_BOX *bound_box = NULL;
+
+	int data_index, i;
     for(data_index=0; data_index< data_list_length; data_index++)
     {
-        coming_data = data_list[data_index];
-        /* do initial when first and second data coming */
+        *coming_data = data_list[data_index];
 
-        if(data_index==0)
+        /* do initial when first and second data coming */
+        if(base_data==NULL)
         {
-            base_data = coming_data;
+            base_data = (struct DATA_POINT *)malloc(sizeof(struct DATA_POINT));
+            *base_data = *coming_data;
+            continue;
+        }
+        else if(bound_box==NULL)
+        {
+            bound_box = (struct BOUND_BOX *)malloc(sizeof(struct BOUND_BOX));
+            initialBoundBox(bound_box, coming_data);
             continue;
         }
 
-        updateCorCircle();
-
-        if(!isThereIntersection(&all_circles, corresponse_circle)||(coming_data.timestamp-base_data.timestamp)>200)
+        if(isTransmitData(bound_box, coming_data, base_data))
         {
-
-            for (fprintf(f_out, "%u", base_data.timestamp),i = 0; i<DIMENSION_WITHOUT_TIMESTAMP; i++)
-                fprintf(f_out, ",%f", base_data.data.coordinate[i]);
+            for(fprintf(f_out, "%u", base_data->timestamp), i=0; i<DIMENSION_WITHOUT_TIMESTAMP; i++)
+                fprintf(f_out, ",%f", base_data->params[i]);
             fprintf(f_out, "\n");
-
-            // update basePoint
-            updateBasePoint();
-
-            // reset all_circle list
-            reInitialList(&all_circles);
-
-            updateCorCircle();
-            addCircleIntoList(&all_circles, corresponse_circle);
-            centre_point = corresponse_circle.centre;
+            
+            updateBasePoint(bound_box, coming_data, base_data);
+            initialBoundBox(bound_box, coming_data);
         }
-        else{
-            reInitialList(&all_circles);
-            all_circles.length = tmp_list_circles.length;
-            all_circles.circles = (struct CIRCLE *)malloc(sizeof(struct CIRCLE) * tmp_list_circles.length);
-            for (i = 0;  i< tmp_list_circles.length; i++) {
-                all_circles.circles[i] = tmp_list_circles.circles[i];
-            }
-        }
+
+        updateBoundBox(bound_box, coming_data, base_data);
     }
 
-    for (fprintf(f_out, "%u", base_data.timestamp),i = 0; i<DIMENSION_WITHOUT_TIMESTAMP; i++)
-        fprintf(f_out, ",%f", base_data.data.coordinate[i]);
+    for(fprintf(f_out, "%u", base_data->timestamp), i=0; i<DIMENSION_WITHOUT_TIMESTAMP; i++)
+        fprintf(f_out, ",%f", base_data->params[i]);
     fprintf(f_out, "\n");
 
-    double ln = coming_data.timestamp - base_data.timestamp;
-    for (fprintf(f_out, "%u", coming_data.timestamp), i = 0; i<DIMENSION_WITHOUT_TIMESTAMP; i++)
-        fprintf(f_out, ",%f", base_data.data.coordinate[i] + (centre_point.coordinate[i]-base_data.data.coordinate[i])*ln);
-	fprintf(f_out, "\n");
+    for(fprintf(f_out, "%u", coming_data->timestamp), i=0; i<DIMENSION_WITHOUT_TIMESTAMP; i++)
+        fprintf(f_out, ",%f", base_data->params[i] + (coming_data->timestamp - base_data->timestamp)*((bound_box->bounds[i][0] + bound_box->bounds[i][1])/2 - base_data->params[i]));
+    fprintf(f_out, "\n");
 
+    free(coming_data);
+    free(base_data);
+    free(bound_box);
 }
-
-
--- makefile
+++ makefile
@@ -1,27 +1,34 @@
 HEADERS = DistinctElement/DoubleLinkedList/DbLinkList.h DistinctElement/DoubleLinkedList/uthash.h
 
-default: cardinality estimation ltc-method ltc-man ltc-eu
+default: cardinality estimation ltc-method ltc-man ltc-eu Experiment-1 Experiment-2
 
 cardinality.o: DistinctElement/DoubleLinkedList/DbLinkList.c  $(HEADERS)
 	gcc -c DistinctElement/DoubleLinkedList/DbLinkList.c -o cardinality.o
 
 main.o: DistinctElement/DoubleLinkedList/main.c $(HEADERS)
-	gcc -c DistinctElement/DoubleLinkedList/main.c -o main.o 
+	gcc -c DistinctElement/DoubleLinkedList/main.c -o main.o
 
 cardinality: cardinality.o main.o
-	gcc cardinality.o main.o -lm -o cardinality 
+	gcc cardinality.o main.o -lm -o cardinality
 
 estimation: DistinctElement/LRU_LC/LRU_LC.c DistinctElement/LRU_LC/LRU_LC.h
 	gcc DistinctElement/LRU_LC/LRU_LC.c -lm -o estimation
-	
+
 ltc-method: DataStreamCompression/LTC/LTC.c DataStreamCompression/LTC/LTC.h
 	gcc DataStreamCompression/LTC/LTC.c DataStreamCompression/LTC/main.c -lm -o LTC-method
 
-ltc-man: 
+ltc-man:
 	gcc DataStreamCompression/LTC-Manhattan/ltc-man.c -o LTC-man
-	
+
 ltc-eu:
 	gcc DataStreamCompression/LTC-Manhattan/ltc-man.c -lm -o LTC-eu
+
+Experiment-1:
+	gcc DataStreamCompression/Experiment1/Infinity-norm.c -lm -o Ex1-infinity
+	gcc DataStreamCompression/Experiment1/Euclidean-norm.c -lm -o Ex1-euclidean
+
+Experiment-2:
+	gcc DataStreamCompression/Experiment2/Infinity-norm.c -lm -o Ex2
 
 clean:
 	-rm -f cardinality.o
@@ -31,3 +38,6 @@ clean:
 	-rm -f LTC-method
 	-rm -f LTC-man
 	-rm -f LTC-eu
+	-rm -f Ex1-infinity
+	-rm -f Ex1-euclidean
+	-rm -f Ex2
